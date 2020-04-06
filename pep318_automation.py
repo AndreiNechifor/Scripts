782e4e92a30e690cc5ed0c134fc2efc30f6c5369
AndreiNechifor <63094300+AndreiNechifor@users.noreply.github.com>
2020-04-06 16:24:01 +0300

    Dynamic review

--- pep318_automation.py
+++ pep318_automation.py
@@ -4,6 +4,9 @@
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 def open_first_link(without_window=False):
@@ -14,47 +17,52 @@ def open_first_link(without_window=False):
     browser = webdriver.Chrome('chromedriver', options=opt)
 
     # Requesting page
-    browser.get('https://www.python.org')
-    search_box = browser.find_element_by_id("id-search-field")  # Search box
-    
-    # Filling the search box with query statement
-    # Sending a letter every half second
-    page=browser.current_url # get the initial page url and match it at line 41
-    print(page)
-    for e in "Decorator":
-        time.sleep(0.5)
-        search_box.send_keys(e)
-    # Perform the search
-    submit_button = browser.find_element_by_id("submit")
-        #Check if the search_box was filled up with "keys".
-    if(search_box.get_attribute!=""):
-        time.sleep(10)
-    submit_button.click()
-    # Asure you landed on the query search page
+    #browser.get('https://www.google.ro')
+    print("Looking for Python.org")
+    browser.get('http://python.org')
+    try: # initial check.
+        main_page=WebDriverWait(browser,10).until(EC.title_is("Welcome to Python.org"))
+        print("Landed on Python main page")
+    except Exception :
+        print("Some error occured")
+    else:
+        search_box = browser.find_element_by_id("id-search-field")  # Search box
+        
+        # Filling the search box with query statement
+        search_box.send_keys("Decorator")
+        # Sending a letter every half second
+        page=browser.current_url # get the initial page url and match it at line 41
+        print(page) 
+        # Perform the search
+        try:
+            submit_button=WebDriverWait(browser,10).until(EC.presence_of_element_located((By.ID,"submit")))
+            print("Found the submit button")
+            submit_button.click()
+        except Exception :
+            print("Some error occured")
 
-    check = browser.find_element_by_tag_name("h2")
-    assert check.text == "Search Python.org"
-    time.sleep(2)
+        # Asure you landed on the query search page
+        check = browser.find_element_by_tag_name("h2")
+        assert check.text == "Search Python.org"
 
-    # Navigating to first search resulted link
-    first_link = browser.find_element_by_link_text(
-        'PEP 318 -- Decorators for Functions and Methods')
-    if(page!=browser.current_url):
+
+        # Navigating to first search resulted link
+        first_link = browser.find_element_by_link_text(
+            'PEP 318 -- Decorators for Functions and Methods')
+        if(page!=browser.current_url):
+            print("URL CHANGED TO ",browser.current_url)
+        first_link.click()
+
+        # Asure you landed on the first link page by matching the expected title with actual page title
+
+        header = browser.find_element_by_class_name("page-title")
+        assert(header.text == 'PEP 318 -- Decorators for Functions and Methods')
+        print("We are in the right page")
+        print("Browser will close in 5 seconds")
         time.sleep(5)
-        print("URL CHANGED TO ",browser.current_url)
-    first_link.click()
 
-    # Asure you landed on the first link page by matching the expected title with actual page title
-
-    header = browser.find_element_by_class_name("page-title")
-    print(header.text)
-    assert(header.text == 'PEP 318 -- Decorators for Functions and Methods')
-    print("We are in the right page")
-    print("Browser will close in 5 seconds")
-    time.sleep(5)
-
-    # Close the browser
-    browser.quit()
+        # Close the browser
+        browser.quit()
 
 open_first_link()
 
