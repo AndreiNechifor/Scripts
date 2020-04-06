5a273d499bf8a51a9d8ae50f3d5e24614a28f69b
AndreiNechifor <63094300+AndreiNechifor@users.noreply.github.com>
2020-04-06 17:12:31 +0300

    Logger

--- pep318_automation.py
+++ pep318_automation.py
@@ -8,39 +8,48 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+from os import curdir
+
+def logging(event,content):
+
+    import time
+
+    logger=open('logger.txt','a+')
+    time_to_write=time.strftime(("%d-%m-%Y %H:%M:%S"))
+    logger.writelines("{0} @ {1}{2}".format(event,time_to_write,"\n"))
+    logger.writelines("\t"*3)
+    logger.writelines(content)
+    logger.write("\n")
+    logger.close()
+
 
 def open_first_link(without_window=False):
-
     # Browser options
     opt = Options()
     opt.headless = without_window
     browser = webdriver.Chrome('chromedriver', options=opt)
 
     # Requesting page
-    #browser.get('https://www.google.ro')
-    print("Looking for Python.org")
     browser.get('http://python.org')
-    try: # initial check.
+    # Initial check.
+    try: 
         main_page=WebDriverWait(browser,10).until(EC.title_is("Welcome to Python.org"))
-        print("Landed on Python main page")
+        logging("Attempting to reach Python.org","Landed on Python.org main page")
     except Exception :
-        print("Some error occured")
+        logging("Attempting to reach Python.org","An error occured")
     else:
         search_box = browser.find_element_by_id("id-search-field")  # Search box
         
         # Filling the search box with query statement
         search_box.send_keys("Decorator")
-        # Sending a letter every half second
-        page=browser.current_url # get the initial page url and match it at line 41
-        print(page) 
         # Perform the search
         try:
             submit_button=WebDriverWait(browser,10).until(EC.presence_of_element_located((By.ID,"submit")))
-            print("Found the submit button")
+            logging("Searching for submit button","Found the submit button")
             submit_button.click()
         except Exception :
-            print("Some error occured")
-
+            logging("Searching for submit button","An error occured")
+        logging("URL CHANGED",browser.current_url)
         # Asure you landed on the query search page
         check = browser.find_element_by_tag_name("h2")
         assert check.text == "Search Python.org"
@@ -49,15 +58,14 @@ def open_first_link(without_window=False):
         # Navigating to first search resulted link
         first_link = browser.find_element_by_link_text(
             'PEP 318 -- Decorators for Functions and Methods')
-        if(page!=browser.current_url):
-            print("URL CHANGED TO ",browser.current_url)
         first_link.click()
 
-        # Asure you landed on the first link page by matching the expected title with actual page title
+        # Assure you landed on the first link page by matching the expected title with actual page title
 
         header = browser.find_element_by_class_name("page-title")
         assert(header.text == 'PEP 318 -- Decorators for Functions and Methods')
-        print("We are in the right page")
+        logging("URL CHANGED",browser.current_url)
+        logging("Search Results","Found <<{0}>>".format(header.text))
         print("Browser will close in 5 seconds")
         time.sleep(5)
 
