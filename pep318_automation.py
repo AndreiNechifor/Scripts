dafd595edc8f0400cd59c1555bee28f8b945a03f
AndreiNechifor <63094300+AndreiNechifor@users.noreply.github.com>
2020-04-07 14:47:00 +0300

    Logger revision

--- pep318_automation.py
+++ pep318_automation.py
@@ -23,8 +23,21 @@ def logging(event,content):
     logger.write("\n")
     logger.close()
 
+def check_presence_by_xpath(xpath,browser):
+    try:
+        x=WebDriverWait(browser,3).until(EC.presence_of_element_located((By.XPATH,xpath)))
+        
+    except TimeoutException:
+        logging("Erorr 01.","Waiting time exceeded")
+        browser.quit()
+    else:
+        return x
+
+cpx=check_presence_by_xpath # shortcut
+
 
 def open_first_link(url,without_window=False):
+
     # Browser options
     opt = Options()
     opt.headless = without_window
@@ -32,47 +45,39 @@ def open_first_link(url,without_window=False):
 
     # Requesting page
     browser.get(url)
+
     # Initial check.
-    try: 
-        main_page=WebDriverWait(browser,10).until(EC.title_is("Welcome to Python.org"))
-        logging("Attempting to reach Python.org","Landed on Python.org main page")
-    except TimeoutException as E :
-        logging("Attempting to reach Python.org","An error occured {0}".format(E))
-    else:
-        search_box = browser.find_element_by_id("id-search-field")  # Search box
+    logo = check_presence_by_xpath('//*[@id="touchnav-wrapper"]/header/div/h1/a/img',browser)
+    
+    search_box = cpx('//*[@id="id-search-field"]',browser)
         
-        # Filling the search box with query statement
-        search_box.send_keys("Decorator")
-        # Perform the search
-        try:
-            submit_button=WebDriverWait(browser,10).until(EC.presence_of_element_located((By.ID,"submit")))
-            logging("Searching for submit button","Found the submit button")
-            submit_button.click()
-        except TimeoutException as E :
-            logging("Searching for submit button","An error occured {0}".format(E))
-        logging("URL CHANGED",browser.current_url)
-        # Asure you landed on the query search page
-
-        check = browser.find_element_by_tag_name("h2")
-        assert check.text == "Search Python.org"
+    # Filling the search box with query statement
+    search_box.send_keys("Decorator")
+    
+    # Perform the search
+    submit_button=cpx('//*[@id="submit"]',browser)
+    submit_button.click()
+    logging("URL CHANGED",browser.current_url)
+    
+    # Asure you landed on the query search page
+    check = browser.find_element_by_tag_name("h2")
+    assert check.text == "Search Python.org"
 
 
-        # Navigating to first search resulted link
-        first_link = browser.find_element_by_link_text(
-            'PEP 318 -- Decorators for Functions and Methods')
-        first_link.click()
+    # Navigating to first search resulted link
+    first_link = browser.find_element_by_xpath('//*[@id="content"]/div/section/form/ul/li[1]/h3/a')
+    first_link.click()
 
-        # Assure you landed on the first link page by matching the expected title with actual page title
+    # Assure you landed on the first link page by matching the expected title with actual page title
+    header = browser.find_element_by_class_name("page-title")
+    assert(header.text == 'PEP 318 -- Decorators for Functions and Methods')
+    logging("URL CHANGED",browser.current_url)
+    logging("Search Results","Found <<{0}>>".format(header.text))
+    print("Browser will close in 5 seconds")
+    time.sleep(5)
+    
+    # Close the browser
+    browser.quit()
 
-        header = browser.find_element_by_class_name("page-title")
-        assert(header.text == 'PEP 318 -- Decorators for Functions and Methods')
-        logging("URL CHANGED",browser.current_url)
-        logging("Search Results","Found <<{0}>>".format(header.text))
-        print("Browser will close in 5 seconds")
-        time.sleep(5)
-
-        # Close the browser
-        browser.quit()
-
-open_first_link('http://google.ro')
+open_first_link('http://python.org')
 
