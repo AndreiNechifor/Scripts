30950a5cb93ad8aa9930c6dc6270fe0edefc6af5
AndreiNechifor <63094300+AndreiNechifor@users.noreply.github.com>
2020-04-07 18:03:27 +0300

    Commit

--- pep318_automation.py
+++ pep318_automation.py
@@ -2,14 +2,14 @@
 # Andrei Nechifor
 # python.org > Search for pep 318 > assert search page > open first link > assert first link page
 
+import time
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
-from os import curdir
 from selenium.common.exceptions import NoSuchElementException,TimeoutException
+from urllib3.exceptions import MaxRetryError
 
 def logging(event,content):
 
@@ -20,24 +20,23 @@ def logging(event,content):
     logger.writelines("{0} @ {1}{2}".format(event,time_to_write,"\n"))
     logger.writelines("\t"*3)
     logger.writelines(content)
-    logger.write("\n")
+    logger.write("{0}{1}{2}".format("\n","-"*50,"\n"))
     logger.close()
 
-def check_presence_by_xpath(xpath,browser):
+def check_presence_by_xpath(xpath,element_name,browser):
+
     try:
         x=WebDriverWait(browser,3).until(EC.presence_of_element_located((By.XPATH,xpath)))
-        
-    except TimeoutException:
-        logging("Erorr 01.","Waiting time exceeded")
+
+    except (TimeoutException,NoSuchElementException,MaxRetryError):
+        logging("Waiting time for {} excedeed".format(element_name),"Browser closed")  
         browser.quit()
     else:
-        return x
-
-cpx=check_presence_by_xpath # shortcut
-
+        return x   
 
 def open_first_link(url,without_window=False):
-
+    
+    cpx=check_presence_by_xpath # shortcut
     # Browser options
     opt = Options()
     opt.headless = without_window
@@ -45,39 +44,56 @@ def open_first_link(url,without_window=False):
 
     # Requesting page
     browser.get(url)
+    logging("URL CHANGED",browser.current_url)
 
-    # Initial check.
-    logo = check_presence_by_xpath('//*[@id="touchnav-wrapper"]/header/div/h1/a/img',browser)
-    
-    search_box = cpx('//*[@id="id-search-field"]',browser)
+    # Assert landing on main page
+    logo = cpx('//*[@id="touchnav-wrapper"]/header/div/h1/a/img','python logo',browser)
+    logging('Looking for Python logo on main page','Python logo found. Continuing the script')
+
+
+    try:
+        # Looking for search box and fill with querry term
+        search_box = cpx('//*[@id="id-search-field"]','search-box',browser)
+        logging('Looking for search-form on main page','Search box found. Continuing the script')
+        search_box.send_keys('Decorator')
+        logging('Fill the search-box with term : Decorator.',' Search box filled. Continuing the script')
         
-    # Filling the search box with query statement
-    search_box.send_keys("Decorator")
-    
-    # Perform the search
-    submit_button=cpx('//*[@id="submit"]',browser)
-    submit_button.click()
-    logging("URL CHANGED",browser.current_url)
-    
-    # Asure you landed on the query search page
-    check = browser.find_element_by_tag_name("h2")
-    assert check.text == "Search Python.org"
+        # Searching for GO button and click it
+        submit_button=cpx('//*[@id="submit"]','GO Button',browser)   
+        submit_button.click()
+        logging("URL CHANGED",browser.current_url)
+        
+        # Asure you landed on the query search page
+        check = browser.find_element_by_tag_name("h2")
+        assert check.text == "Search Python.org"
 
 
-    # Navigating to first search resulted link
-    first_link = browser.find_element_by_xpath('//*[@id="content"]/div/section/form/ul/li[1]/h3/a')
-    first_link.click()
+        # Navigating to first search resulted link
+        first_link = browser.find_element_by_xpath('//*[@id="content"]/div/section/form/ul/li[1]/h3/a')
+        first_link.click()
+        logging("URL CHANGED",browser.current_url)
 
-    # Assure you landed on the first link page by matching the expected title with actual page title
-    header = browser.find_element_by_class_name("page-title")
-    assert(header.text == 'PEP 318 -- Decorators for Functions and Methods')
-    logging("URL CHANGED",browser.current_url)
-    logging("Search Results","Found <<{0}>>".format(header.text))
-    print("Browser will close in 5 seconds")
-    time.sleep(5)
-    
-    # Close the browser
-    browser.quit()
+        # Assure you landed on the first link page by matching the expected title with actual page title
+        header = browser.find_element_by_class_name("page-title")
+        assert(header.text == 'PEP 318 -- Decorators for Functions and Methods')
+        logging("URL CHANGED",browser.current_url)
+        logging("Search Results","Found <<{0}>>".format(header.text))
+        if(without_window==False):
+            print("Browser will close in 5 seconds")
+            time.sleep(5)
+    except AttributeError as Error:
+        logging('One of the requested elements does not belong to this page.','Code error %s' % Error)
+        return 0
+    finally:
+        # Close the browser
+        browser.quit()
+        return 1
 
-open_first_link('http://python.org')
-
+# Testing
+test_pages=['http://google.ro','http://python.org','http://www.bitacad.net']
+succes_counter=0
+for e in test_pages:
+    succes_counter=open_first_link(e,without_window=True)
+succes_rate=float(succes_counter)/len(test_pages)*100
+logging('Test finished','Succes rate : {0}%'.format(succes_counter))
+print('Test finished')
