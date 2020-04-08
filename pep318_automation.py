218634b1657f1dc825eb8fad176fc1c50be383df
AndreiNechifor <63094300+AndreiNechifor@users.noreply.github.com>
2020-04-08 15:48:54 +0300

    Except attribute error, maxretry, timeoutrror

--- pep318_automation.py
+++ pep318_automation.py
@@ -5,12 +5,12 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common import action_chains,keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException,TimeoutException
-from urllib3.exceptions import MaxRetryError
-
+from urllib3 import poolmanager,exceptions
 def logging(event,content):
 
     import time
@@ -26,10 +26,14 @@ def logging(event,content):
 def check_presence_by_xpath(xpath,element_name,browser):
 
     try:
-        x=WebDriverWait(browser,3).until(EC.presence_of_element_located((By.XPATH,xpath)))
+        x=WebDriverWait(browser,5).until(EC.presence_of_element_located((By.XPATH,xpath)))
+        logging('Looking for %s on %s page.' %(element_name,browser.current_url),'Continuing the script')
 
-    except (TimeoutException,NoSuchElementException,MaxRetryError):
+    except TimeoutException as e:
         logging("Waiting time for {} excedeed".format(element_name),"Browser closed")  
+        browser.quit()
+    except Exception:
+        loggin('Unkown error',"")
         browser.quit()
     else:
         return x   
@@ -43,20 +47,24 @@ def open_first_link(url,without_window=False):
     browser = webdriver.Chrome('chromedriver', options=opt)
 
     # Requesting page
-    browser.get(url)
+    try:
+        browser.get(url)
+        r=poolmanager.PoolManager()
+        r.request('GET',url)
+
+    except exceptions.MaxRetryError as e:
+        logging('Connection ERROR:\n%s'%(str(e)),'Connection could not be esteablished')
+        browser.quit()
+        exit()
     logging("URL CHANGED",browser.current_url)
 
     # Assert landing on main page
     logo = cpx('//*[@id="touchnav-wrapper"]/header/div/h1/a/img','python logo',browser)
-    logging('Looking for Python logo on main page','Python logo found. Continuing the script')
-
 
     try:
         # Looking for search box and fill with querry term
         search_box = cpx('//*[@id="id-search-field"]','search-box',browser)
-        logging('Looking for search-form on main page','Search box found. Continuing the script')
         search_box.send_keys('Decorator')
-        logging('Fill the search-box with term : Decorator.',' Search box filled. Continuing the script')
         
         # Searching for GO button and click it
         submit_button=cpx('//*[@id="submit"]','GO Button',browser)   
@@ -83,14 +91,12 @@ def open_first_link(url,without_window=False):
             time.sleep(5)
     except AttributeError as Error:
         logging('One of the requested elements does not belong to this page.','Code error %s' % Error)
-        return 0
     finally:
         # Close the browser
         browser.quit()
-        return 1
-
+#open_first_link('C:\\Users\\AMnechifor\\Desktop\\py\\Welcome to Python.org.html')
 # Testing
-test_pages=['http://google.ro','http://python.org','http://www.bitacad.net']
+test_pages=['http://scratchpd.com','http://google.ro','http://python.org','http://www.bitacad.net']
 succes_counter=0
 for e in test_pages:
     succes_counter=open_first_link(e,without_window=True)
